5f34042d6998f77543d7ccd6af8ebb6732b2569f
PsionicGeek <rrkumar220920@gmail.com>
2024-11-25 00:53:36 +0530

    Initial commit

---
+++ src/main/java/org/psionicgeek/uber_backend/UberBackendApplication.java
@@ -0,0 +1,13 @@
+package org.psionicgeek.uber_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UberBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/psionicgeek/uber_backend/UberBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.psionicgeek.uber_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
